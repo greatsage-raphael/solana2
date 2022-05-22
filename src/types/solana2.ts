40dd1fbda36526087fddf041921324e225252064
Collins Lubwama <bizzicole87@gmail.com>
2022-05-22 06:27:04 +0300

    Anchor-solana

---
+++ src/types/solana2.ts
@@ -0,0 +1,23 @@
+export type Solana2 = {
+  "version": "0.1.0",
+  "name": "solana2",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [],
+      "args": []
+    }
+  ]
+};
+
+export const IDL: Solana2 = {
+  "version": "0.1.0",
+  "name": "solana2",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [],
+      "args": []
+    }
+  ]
+};
